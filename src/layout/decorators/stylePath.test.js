75b9197bb437a61916531fd3ca8a3d642cb994d9
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-19 21:39:56 +0200

    test: test if stylePath README example works 2/2

--- src/layout/decorators/stylePath.test.js
+++ src/layout/decorators/stylePath.test.js
@@ -1,4 +1,4 @@
-const {line, stylePath} = require('../..')
+const {line, stylePath, table} = require('../..')
 
 test('stylePath README example works 1/2', () => {
   const style = {
@@ -12,6 +12,24 @@ test('stylePath README example works 1/2', () => {
   const res = stylePath(['line'], pad4)(answer42)(style)
 
   const exp = '    The answer is 42.                   \n'
+
+  expect(res).toStrictEqual(exp)
+})
+
+test('stylePath README example works 2/2', () => {
+  const style = {
+    cols: [{width: 20}, {width: 20}]
+  }
+
+  const pad4 = obj => ({...obj, padStart: (obj.padStart || 0) + 4, width: obj.width - 4})
+
+  const answer42 = table([
+    ['--answer', '42']
+  ])
+
+  const res = stylePath(['cols', 0], pad4)(answer42)(style)
+
+  const exp = '    --answer        42                  \n'
 
   expect(res).toStrictEqual(exp)
 })
