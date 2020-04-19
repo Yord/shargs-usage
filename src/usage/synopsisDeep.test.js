276110007904af4b236531e8c7acb583fb8d8c57
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-19 21:38:17 +0200

    test: test if synopsisDeepFrom with undefined id uses line

--- src/usage/synopsisDeep.test.js
+++ src/usage/synopsisDeep.test.js
@@ -307,3 +307,47 @@ test('synopsisDeep with wrong id uses default style', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('synopsisDeepFrom with undefined id uses line', () => {
+  const commandOpts = [
+    undefined,
+    {foo: 'bar'                                                                          },
+    {                    args: ['-w', '--wrong']                                         },
+    {                                                types: ['wrong']                    },
+    {                                                                           opts: [] },
+    {key: 'stringPos',                               types: ['string']                   },
+    {key: 'numberPos',                               types: ['number']                   },
+    {key: 'boolPos',                                 types: ['bool']                     },
+    {key: 'arrayPos',                                types: ['bool', 'bool']             },
+    {key: 'variadicPos'                                                                  },
+    {key: 'command',     args: [],                                               opts: []},
+    {key: 'variadic',    args: ['-v', '--variadic']                                      },
+    {key: 'flag',        args: ['-f', '--flag'],     types: []                           },
+    {key: 'string',      args: ['-s', '--string'],   types: ['string']                   },
+    {key: 'number',      args: ['-n', '--number'],   types: ['number']                   },
+    {key: 'bool',        args: ['-b', '--bool'],     types: ['bool']                     },
+    {key: 'array',       args: ['-a', '--array'],    types: ['string', 'number']         }
+  ]
+
+  const opts = [
+    {key: 'withOpts', args: ['with-opts'], opts: commandOpts},
+    ...commandOpts
+  ]
+
+  const style = {
+    line: {width: 75}
+  }
+
+  const res = synopsisDeepFrom()('deepThought')(opts)(style)
+
+  const txt = 'deepThought [<stringPos>] [<numberPos>] [<boolPos>] [<arrayPos>]           \n' +
+              '            [<variadicPos>...] [-v|--variadic] [-f|--flag] [-s|--string]   \n' +
+              '            [-n|--number] [-b|--bool] [-a|--array]                         \n' +
+              'deepThought with-opts [<stringPos>] [<numberPos>] [<boolPos>] [<arrayPos>] \n' +
+              '                      [<variadicPos>...] [-v|--variadic] [-f|--flag]       \n' +
+              '                      [-s|--string] [-n|--number] [-b|--bool] [-a|--array] \n' +
+              'deepThought with-opts command                                              \n' +
+              'deepThought command                                                        \n'
+
+  expect(res).toStrictEqual(txt)
+})
