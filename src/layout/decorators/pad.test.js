b3423143e79e4bcdf9297b070658463b021dbd44
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-19 21:44:42 +0200

    test: test if pad works on arrays

--- src/layout/decorators/pad.test.js
+++ src/layout/decorators/pad.test.js
@@ -1,4 +1,4 @@
-const {line, pad} = require('../..')
+const {line, pad, table} = require('../..')
 
 test('pad README example works', () => {
   const style = {
@@ -10,6 +10,20 @@ test('pad README example works', () => {
   const res = pad(['line'], 4)(answer42)(style)
 
   const exp = '    The answer is 42.                   \n'
+
+  expect(res).toStrictEqual(exp)
+})
+
+test('pad works on arrays', () => {
+  const style = {
+    cols: [{width: 20}, {width: 20}]
+  }
+  
+  const answer42 = table([['--answer', '42']])
+
+  const res = pad(['cols', 0], 4)(answer42)(style)
+
+  const exp = '    --answer        42                  \n'
 
   expect(res).toStrictEqual(exp)
 })
