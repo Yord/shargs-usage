e9fda5f69554f540055ec68d10a27b0c0358c01b
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-19 17:49:23 +0200

    test: test if synopsisDeep uses default line style if style is undefined

--- src/usage/synopsisDeep.test.js
+++ src/usage/synopsisDeep.test.js
@@ -1,4 +1,4 @@
-const {synopsisDeep} = require('..')
+const {synopsisDeep, synopsisDeepFrom} = require('..')
 
 test('synopsisDeep README example works as expected', () => {
   const askOpts = [
@@ -166,6 +166,46 @@ test('synopsisDeep uses default line style if line is undefined in style', () =>
   const style = {}
 
   const res = synopsisDeep('deepThought')(opts)(style)
+
+  const txt = 'deepThought [<stringPos>] [<numberPos>] [<boolPos>] [<arrayPos>]                \n' +
+              '            [<variadicPos>...] [-v|--variadic] [-f|--flag] [-s|--string]        \n' +
+              '            [-n|--number] [-b|--bool] [-a|--array]                              \n' +
+              'deepThought with-opts [<stringPos>] [<numberPos>] [<boolPos>] [<arrayPos>]      \n' +
+              '                      [<variadicPos>...] [-v|--variadic] [-f|--flag]            \n' +
+              '                      [-s|--string] [-n|--number] [-b|--bool] [-a|--array]      \n' +
+              'deepThought with-opts co                                                        \n' +
+              'deepThought co                                                                  \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
+test('synopsisDeep uses default line style if style is undefined', () => {
+  const commandOpts = [
+    undefined,
+    {foo: 'bar'                                                                          },
+    {                    args: ['-w', '--wrong']                                         },
+    {                                                types: ['wrong']                    },
+    {                                                                           opts: [] },
+    {key: 'stringPos',                               types: ['string']                   },
+    {key: 'numberPos',                               types: ['number']                   },
+    {key: 'boolPos',                                 types: ['bool']                     },
+    {key: 'arrayPos',                                types: ['bool', 'bool']             },
+    {key: 'variadicPos'                                                                  },
+    {key: 'command',     args: ['co', 'command'],                                opts: []},
+    {key: 'variadic',    args: ['-v', '--variadic']                                      },
+    {key: 'flag',        args: ['-f', '--flag'],     types: []                           },
+    {key: 'string',      args: ['-s', '--string'],   types: ['string']                   },
+    {key: 'number',      args: ['-n', '--number'],   types: ['number']                   },
+    {key: 'bool',        args: ['-b', '--bool'],     types: ['bool']                     },
+    {key: 'array',       args: ['-a', '--array'],    types: ['string', 'number']         }
+  ]
+
+  const opts = [
+    {key: 'withOpts', args: ['with-opts'], opts: commandOpts},
+    ...commandOpts
+  ]
+
+  const res = synopsisDeep('deepThought')(opts)()
 
   const txt = 'deepThought [<stringPos>] [<numberPos>] [<boolPos>] [<arrayPos>]                \n' +
               '            [<variadicPos>...] [-v|--variadic] [-f|--flag] [-s|--string]        \n' +
