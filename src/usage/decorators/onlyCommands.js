fedfbd5d71c0e1652006633edaf03ce8d00e2e1a
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-19 17:36:27 +0200

    test: test onlyCommands for options that are undefined

--- src/usage/decorators/onlyCommands.js
+++ src/usage/decorators/onlyCommands.js
@@ -1,7 +1,7 @@
 const optsFilter = require('./optsFilter')
 
 module.exports = optsFilter(
-  ({key, args, types, opts}) => (
+  ({key, args, types, opts} = {}) => (
     typeof key !== 'undefined' &&
     Array.isArray(args) &&
     typeof types === 'undefined' &&
