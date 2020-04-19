f635e24fcbf78f3edf0faad4339e2264fd31f689
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-19 21:46:24 +0200

    test: test if optsListDeep works with all option types

--- src/usage/optsListDeep.test.js
+++ src/usage/optsListDeep.test.js
@@ -26,3 +26,70 @@ test('optsListDeep README example works as expected', () => {
   
   expect(res).toStrictEqual(txt)
 })
+
+test('optsListDeep works with all option types', () => {
+  const varOpts = [
+    {key: 'variadicPos', desc: 'Variadic.' },
+  ]
+
+  const commandOpts = [
+    undefined,
+    {foo: 'bar'                                                                                                                                          },
+    {                    args: ['-w', '--wrong']                                                                                                         },
+    {                                                types: ['wrong']                                                                                    },
+    {                                                                           opts: []                                                                 },
+    {key: 'stringPos',                               types: ['string'],                         desc: 'String positional argument.'                      },
+    {key: 'numberPos',                               types: ['number'],                         desc: 'Number positional argument.'                      },
+    {key: 'boolPos',                                 types: ['bool'],                           desc: 'Bool positional argument.'                        },
+    {key: 'arrayPos',                                types: ['bool', 'bool'],                   desc: 'Array positional argument.'                       },
+    {key: 'variadicPos',                                                                        desc: 'Variadic positional argument.'                    },
+    {key: 'command',     args: ['command'],                                      opts: varOpts, desc: 'Command option.'                                  },
+    {key: 'variadic',    args: ['-v', '--variadic'],                                            desc: 'Variadic option.'                                 },
+    {key: 'flag',        args: ['-f', '--flag'],     types: [],                                 desc: 'Flag option.'                                     },
+    {key: 'string',      args: ['-s', '--string'],   types: ['string'],                         desc: 'String option.'                                   },
+    {key: 'number',      args: ['-n', '--number'],   types: ['number'],                         desc: 'Number option.'                                   },
+    {key: 'bool',        args: ['-b', '--bool'],     types: ['bool'],                           desc: 'Bool option.'                                     },
+    {key: 'array',       args: ['-a', '--array'],    types: ['string', 'number'],               desc: 'Array option.',                 descArg: 'str num'}
+  ]
+
+  const opts = [
+    {key: 'withOpts', args: ['with-opts'], opts: commandOpts, desc: 'With-opts command.'},
+    ...commandOpts
+  ]
+
+  const style = {
+    cols: [{width: 35}, {width: 30}]
+  }
+
+  const res = optsListDeep(opts)(style)
+
+  const txt = '<stringPos>                        String positional argument.   \n' +
+              '<numberPos>                        Number positional argument.   \n' +
+              '<boolPos>                          Bool positional argument.     \n' +
+              '<arrayPos>                         Array positional argument.    \n' +
+              '<variadicPos>...                   Variadic positional argument. \n' +
+              '-v, --variadic                     Variadic option.              \n' +
+              '-f, --flag                         Flag option.                  \n' +
+              '-s, --string=<string>              String option.                \n' +
+              '-n, --number=<number>              Number option.                \n' +
+              '-b, --bool=<bool>                  Bool option.                  \n' +
+              '-a, --array=<str num>              Array option.                 \n' +
+              'with-opts                          With-opts command.            \n' +
+              '    <stringPos>                    String positional argument.   \n' +
+              '    <numberPos>                    Number positional argument.   \n' +
+              '    <boolPos>                      Bool positional argument.     \n' +
+              '    <arrayPos>                     Array positional argument.    \n' +
+              '    <variadicPos>...               Variadic positional argument. \n' +
+              '    -v, --variadic                 Variadic option.              \n' +
+              '    -f, --flag                     Flag option.                  \n' +
+              '    -s, --string=<string>          String option.                \n' +
+              '    -n, --number=<number>          Number option.                \n' +
+              '    -b, --bool=<bool>              Bool option.                  \n' +
+              '    -a, --array=<str num>          Array option.                 \n' +
+              '    command                        Command option.               \n' +
+              '        <variadicPos>...           Variadic.                     \n' +
+              'command                            Command option.               \n' +
+              '    <variadicPos>...               Variadic.                     \n'
+
+  expect(res).toStrictEqual(txt)
+})
