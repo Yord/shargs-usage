e33c67117e9505381a441e66db964286fb399ac1
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-25 19:07:19 +0200

    feat: add types for br and brFrom

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,5 @@
+export {br, brFrom}               from './layout/br'
+
 export interface Opt {
   [key: string]: any
 }
---
+++ src/layout/br.d.ts
@@ -0,0 +1,4 @@
+import {Style} from '../style'
+
+export const br:                     (style?: Style) => string
+export const brFrom: (id: string) => (style?: Style) => string
