7e29b89966f29157abf128ca83e15797d3b08ce0
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-25 22:43:00 +0200

    chore: update functions signature in usage

--- src/usage/combinators/usage.d.ts
+++ src/usage/combinators/usage.d.ts
@@ -2,6 +2,6 @@ import {Opt}   from '../..';
 import {Style} from '../../style';
 
 export const usage: (functions?: ((opts?: Opt[]) => (style?: Style) => string)[]) =>
-                    (opts?: object[]) =>
+                    (opts?: Opt[]) =>
                     (style?: Style) =>
                     string
