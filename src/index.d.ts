7e118c1952463d385436a81aacbb52aed08e5a65
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-25 19:08:21 +0200

    feat: add types for decorate

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,7 @@
 export {br, brFrom}               from './layout/br'
 export {brs, brsFrom}             from './layout/brs'
 export {cols, colsFrom}           from './layout/cols'
+export {decorate}                 from './combinators/decorate'
 
 export interface Opt {
   [key: string]: any
