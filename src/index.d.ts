2d8c200d93bfc6aca7f86720e3eea41788af368f
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-25 19:09:20 +0200

    feat: add types for layout

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,6 +4,7 @@ export {cols, colsFrom}           from './layout/cols'
 export {decorate}                 from './combinators/decorate'
 export {defs, defsFrom}           from './layout/defs'
 export {justArgs}                 from './usage/decorators/justArgs'
+export {layout}                   from './layout/combinators/layout'
 
 export interface Opt {
   [key: string]: any
