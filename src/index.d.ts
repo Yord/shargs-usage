012f6646e9ba6e559f46f3916da21fda1c4d073f
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-25 19:08:36 +0200

    feat: add types for defs and defsFrom

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,7 @@ export {br, brFrom}               from './layout/br'
 export {brs, brsFrom}             from './layout/brs'
 export {cols, colsFrom}           from './layout/cols'
 export {decorate}                 from './combinators/decorate'
+export {defs, defsFrom}           from './layout/defs'
 
 export interface Opt {
   [key: string]: any
