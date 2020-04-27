4c9eddf191d4cab8d5d9514b6b524e2ad286ff09
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-27 10:45:05 +0200

    feat: cut off line if width is shorter than a string's length

--- src/layout/line.js
+++ src/layout/line.js
@@ -1,10 +1,12 @@
 const {defaultStyle} = require('../style')
 
-// TODO: cut off string if it surpasses the line's width
 const lineFrom = id => (string = '') => (
-  ({[id]: line = defaultStyle.line} = defaultStyle) => (
-    ''.padStart(line[0] && line[0].padStart || 0) + string.padEnd(line[0] && line[0].width) + '\n'
-  )
+  ({[id]: line = defaultStyle.line} = defaultStyle) => {
+    const styleObj = line[0] || {padStart: 0, width: 80}
+    const {padStart = 0, width = 80} = styleObj
+
+    return ''.padStart(padStart) + string.slice(0, width).padEnd(width) + '\n'
+  }
 )
 
 const line = lineFrom('line')
