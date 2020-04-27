b2fb4e5ddf83a9d8dbc01f2294ec5ed19981a01c
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-27 17:47:31 +0200

    test: test if desc uses default style if style is undefined

--- src/usage/desc.test.js
+++ src/usage/desc.test.js
@@ -33,3 +33,20 @@ test('desc returns an empty line if no descArg is given', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('desc uses default style if style is undefined', () => {
+  const opts = {
+    opts: [],
+    desc: (
+      'Deep Thought was created to come up with the Answer to ' +
+      'The Ultimate Question of Life, the Universe, and Everything.'
+    )
+  }
+
+  const res = desc(opts)()
+
+  const txt = 'Deep Thought was created to come up with the Answer to The Ultimate Question of \n' +
+              'Life, the Universe, and Everything.                                             \n'
+
+  expect(res).toStrictEqual(txt)
+})
