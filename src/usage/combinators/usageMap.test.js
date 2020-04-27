fb386df8225f7df494d2a3363df0e925ad852d8b
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-27 16:42:57 +0200

    test: test if optsTable README example works as expected

--- src/usage/combinators/usageMap.test.js
+++ src/usage/combinators/usageMap.test.js
@@ -163,3 +163,31 @@ test('usageMap returns empty string if function is undefined', () => {
 
   expect(res).toStrictEqual(txt)
 })
+
+test('optsTable README example works as expected', () => {
+  const optsTable = usageMap(
+    ({key, args, required, desc}) => table([
+      [(required ? '*' : '') + key, args.join(', '), desc]
+    ])
+  )
+
+  const opts = {
+    opts: [
+      {key: 'answer', types: ['number'], args: ['-a', '--answer'], desc: 'The answer.', required: true},
+      {key: 'help', opts: [], args: ['-h', '--help'], desc: 'Prints help.'},
+      {key: 'version', types: [], args: ['--version'], desc: 'Prints version.'}
+    ]
+  }
+
+  const style = {
+    cols: [{width: 10}, {width: 15}, {width: 15}]
+  }
+  
+  const res = optsTable(opts)(style)
+
+  const txt = '*answer   -a, --answer   The answer.    \n' +
+              'help      -h, --help     Prints help.   \n' +
+              'version   --version      Prints version.\n'
+
+  expect(res).toStrictEqual(txt)
+})
