fcd3fbccabe872eeaaf5a73498ec3328b900cb74
Philipp Wille <philipp.y.wille@gmail.com>
2020-12-07 22:40:45 +0100

    test: test if synopses does not print a prefix if a subcommands key is the empty string

--- src/usage/synopses.test.js
+++ src/usage/synopses.test.js
@@ -88,6 +88,53 @@ test('synopses with undefined opt prints an empty line', () => {
   expect(res).toStrictEqual(txt)
 })
 
+test('synopses does not print a prefix if a subcommands key is the empty string', () => {
+  const commandOpts = [
+    undefined,
+    {foo: 'bar'},
+    {                    args: ['-w', '--wrong']                                         },
+    {                                                types: ['wrong']                    },
+    {                                                                           opts: [] },
+    {key: 'stringPos',                               types: ['string']                   },
+    {key: 'numberPos',                               types: ['number']                   },
+    {key: 'boolPos',                                 types: ['bool']                     },
+    {key: 'arrayPos',                                types: ['bool', 'bool']             },
+    {key: 'variadicPos'                                                                  },
+    {key: 'command',     args: [],                                               opts: []},
+    {key: 'variadic',    args: ['-v', '--variadic']                                      },
+    {key: 'flag',        args: ['-f', '--flag'],     types: []                           },
+    {key: 'string',      args: ['-s', '--string'],   types: ['string']                   },
+    {key: 'number',      args: ['-n', '--number'],   types: ['number']                   },
+    {key: 'bool',        args: ['-b', '--bool'],     types: ['bool']                     },
+    {key: 'array',       args: ['-a', '--array'],    types: ['string', 'number']         }
+  ]
+
+  const opts = {
+    key: 'deepThought',
+    opts: [
+      {key: '', args: ['with-opts'], opts: commandOpts},
+      ...commandOpts
+    ]
+  }
+
+  const style = {
+    line: [{width: 80}]
+  }
+
+  const res = synopses(opts)(style)
+
+  const txt = 'deepThought [<stringPos>] [<numberPos>] [<boolPos>] [<arrayPos>]                \n' +
+              '            [<variadicPos>...] [-v|--variadic] [-f|--flag] [-s|--string]        \n' +
+              '            [-n|--number] [-b|--bool] [-a|--array]                              \n' +
+              'deepThought [<stringPos>] [<numberPos>] [<boolPos>] [<arrayPos>]                \n' +
+              '            [<variadicPos>...] [-v|--variadic] [-f|--flag] [-s|--string]        \n' +
+              '            [-n|--number] [-b|--bool] [-a|--array]                              \n' +
+              'deepThought command                                                             \n' +
+              'deepThought command                                                             \n'
+
+  expect(res).toStrictEqual(txt)
+})
+
 test('synopses works without programName', () => {
   const commandOpts = [
     undefined,
