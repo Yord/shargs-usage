fba3e71f9faac058a3a2014430953fc4891425d1
Philipp Wille <philipp.y.wille@gmail.com>
2021-01-31 22:05:59 +0100

    test: test if optsF generates default shields for numbers

--- src/usage/combinators/optsF.test.js
+++ src/usage/combinators/optsF.test.js
@@ -35,3 +35,20 @@ test('optsF generates required shields', () => {
 
   expect(opts).toStrictEqual(opt.opts)
 })
+
+test('optsF generates default shields for numbers', () => {
+  const answer = {key: 'answer', types: ['number'], args: ['-a', '--answer'], defaultValues: [42]}
+
+  const opt = {
+    opts: [
+      answer
+    ]
+  }
+
+  const {defArgs, descOpt, opts} = optsF(f)(opt)
+
+  expect(defArgs(answer)).toStrictEqual('-a, --answer=<number>')
+  expect(descOpt(answer)).toStrictEqual(' [default: 42]')
+
+  expect(opts).toStrictEqual(opt.opts)
+})
