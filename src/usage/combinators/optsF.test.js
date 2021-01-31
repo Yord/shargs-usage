f56d0218c72396817347e0380ec880b70fa46700
Philipp Wille <philipp.y.wille@gmail.com>
2021-01-31 22:05:47 +0100

    test: test if optsF generates required shields

--- src/usage/combinators/optsF.test.js
+++ src/usage/combinators/optsF.test.js
@@ -18,3 +18,20 @@ test('optsF sorts args', () => {
 
   expect(opts).toStrictEqual(opt.opts)
 })
+
+test('optsF generates required shields', () => {
+  const answer = {key: 'answer', types: ['number'], args: ['-a', '--answer'], required: true}
+
+  const opt = {
+    opts: [
+      answer
+    ]
+  }
+
+  const {defArgs, descOpt, opts} = optsF(f)(opt)
+
+  expect(defArgs(answer)).toStrictEqual('-a, --answer=<number>')
+  expect(descOpt(answer)).toStrictEqual(' [required]')
+
+  expect(opts).toStrictEqual(opt.opts)
+})
